ca4fc306ef2a332f2976bdfcb346b3a77a09a007
harshau9 <harshauv9@gmail.com>
2024-04-04 16:04:14 +0530

    main backend for all projects

--- configs/db.js
+++ configs/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 
-
-const  connection = mongoose.connect(process.env.mongoURL)
-
-module.exports = {
-  connection
+const connectDB = async() => {
+  try {
+    const  connect =await mongoose.connect(process.env.mongoURL,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(`MongoDB Connected: ${connect.connection.host}`)
+  } catch (error) {
+    console.log(`Error connecting to MongoDB: ${error.message}`)
+    process.exit()
+  }
 }
+
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 require("dotenv").config();
-const { connection } = require("./configs/db");
+const connectDB = require("./configs/db")
 const userRoutes = require("./routes/userRoutes")
 const categoryRoutes = require("./routes/categoryRoutes")
 const productRoutes = require("./routes/productRoutes")
 const cors=require("cors");
 const app =express();
 
-app.use(cors({
-  origin:"*"
-}))
+app.use(cors());
 app.use(express.json());
+connectDB()
 app.get("/", (req, res) => {
   res.send("HOMEPAGE");
 });
@@ -20,13 +19,5 @@ app.use(`/user`, userRoutes)
 app.use('/categories', categoryRoutes);
 app.use('/products', productRoutes);
 
-app.listen(8080, async()=>{
-  try {
-    await connection
-    console.log("Connected to DB")
-  } catch (err) {
-    console.log("Error connecting to DB")
-    console.log(err)
-  }
-  console.log("server is running on port 8080");
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
