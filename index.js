efd9fe7073a3af56b990fa25816a8aa2e3572798
harshau9 <harshauv9@gmail.com>
2024-04-04 15:01:11 +0530

    main backend for all projects

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./configs/db")
+const { connection } = require("./configs/db");
 const userRoutes = require("./routes/userRoutes")
 const categoryRoutes = require("./routes/categoryRoutes")
 const productRoutes = require("./routes/productRoutes")
-
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
 const cors=require("cors");
+
 dotenv.config()
 connectDB()
 const app =express();
@@ -24,4 +23,13 @@ app.use(`/user`, userRoutes)
 app.use('/categories', categoryRoutes);
 app.use('/products', productRoutes);
 
-app.listen(8080, console.log(`server is running on port 8080`));
+app.listen(8080, async()=>{
+  try {
+    await connection
+    console.log("Connected to DB")
+  } catch (err) {
+    console.log("Error connecting to DB")
+    console.log(err)
+  }
+  console.log("server is running on port 8080");
+})
