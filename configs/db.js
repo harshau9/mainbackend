63cf33ea33668a75405d7cf98d2a604f199ab48e
harshau9 <harshauv9@gmail.com>
2024-04-04 14:32:48 +0530

    main backend for all projects

---
+++ configs/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require("dotenv").config()
+
+const connectDB = async() => {
+  try {
+    const  connect =await mongoose.connect(process.env.mongoURL)
+    console.log(`MongoDB Connected: ${connect.connection.host}`)
+  } catch (error) {
+    console.log(`Error connecting to MongoDB: ${error.message}`)
+    process.exit()
+  }
+}
+
+module.exports = connectDB
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./configs/db")
+const userRoutes = require("./routes/userRoutes")
+const categoryRoutes = require("./routes/categoryRoutes")
+const productRoutes = require("./routes/productRoutes")
+
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
+const cors=require("cors");
+dotenv.config()
+connectDB()
+const app =express();
+
+app.use(cors({
+  origin:"*"
+}))
+app.use(express.json());
+const PORT  = process.env.PORT || 8080;
+
+app.use(`/user`, userRoutes)
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
+
+// middlewares to handle error messages
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT, console.log(`server is running on port ${PORT}`));
